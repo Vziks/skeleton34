b07343494ee2494489b4a006bd2f8f0b97fc96f8
Anton Prokhorov <vziks@live.ru>
2018-06-17 00:14:08 +0300

    lets try with database

--- bin/setup-test.sh
+++ bin/setup-test.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-cp phpunit.xml.dist phpunit.xml
+if [ ! -f app/phpunit.xml ]; then
+    cp app/phpunit.xml.dist app/phpunit.xml
+fi
 
-bin/console doctrine:schema:drop --force -e test
-bin/console doctrine:schema:update --force -e test
-#bin/console doctrine:fixtures:load -n -e test
-bin/console sonata:media:fix-media-context -e test
+rm -f app/logs/**/*.log app/logs/*.log
+
+
+php app/console doctrine:schema:drop --force -e test
+php app/console doctrine:migrations:migrate --no-interaction -e test
+#php app/console doctrine:fixtures:load -n -e test
+php app/console sonata:media:fix-media-context -e test
+php app/console assets:install --symlink web -e test
